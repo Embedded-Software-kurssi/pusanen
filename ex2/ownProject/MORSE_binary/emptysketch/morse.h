2931530c1e79c1f1d896c75f81b5c040e972522e
Jarmo P <jarmo.pesonen@edu.turkuamk.fi>
2016-09-20 13:13:14 +0300

    Update ownProject

---
+++ ex2/ownProject/MORSE_binary/emptysketch/morse.h
@@ -0,0 +1,16 @@
+/*
+ * morse.h
+ *
+ * Created: 20.9.2016 12.28.57
+ *  Author: pusan
+ */ 
+
+
+#ifndef MORSE_H_
+#define MORSE_H_
+
+void bitAction(char b);
+void showText(const char* text);
+
+
+#endif /* MORSE_H_ */
