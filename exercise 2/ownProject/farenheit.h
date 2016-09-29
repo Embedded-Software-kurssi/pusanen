07bccda870b3b250de6115059976353f58f5e478
Jarmo P <jarmo.pesonen@edu.turkuamk.fi>
2016-09-29 14:00:49 +0300

    ex3t6

---
+++ exercise 2/ownProject/farenheit.h
@@ -0,0 +1,17 @@
+/*
+ * farenheit.h
+ *
+ * Created: 29.9.2016 10.29.55
+ *  Author: Ilmeister
+ */ 
+
+
+#ifndef FARENHEIT_H_
+#define FARENHEIT_H_
+
+float toFarenheit (float celsius);
+float toCelsius (float farenheit);
+
+
+
+#endif /* FARENHEIT_H_ */
