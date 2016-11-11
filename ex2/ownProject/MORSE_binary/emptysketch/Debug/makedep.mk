de696b25ba3d9434c61051956b9c668ae7904533
Jarmo P <jarmo.pesonen@edu.turkuamk.fi>
2016-11-11 12:17:31 +0200

    exercise2 files.

--- ex2/ownProject/MORSE_binary/emptysketch/Debug/makedep.mk
+++ ex2/ownProject/MORSE_binary/emptysketch/Debug/makedep.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-morse.cpp
-
 Sketch.cpp
+
+morse.cpp
 
---
+++ ex2/ownProject/MORSE_binary/emptysketch/Sketch.cpp
@@ -0,0 +1,21 @@
+
+/*
+ * main.cpp
+ *
+ * Created: 29.9.2016 11.46.08
+ *  Author: Ilmeister
+ */ 
+
+
+ void setup(){
+
+
+ }
+
+ void loop(){
+
+ }
+
+
+
+ 
