6f9d64cc8e186d8a3ef561c53ddd2bf04ad0ed0f
jaska <kissak@kavelee.abc>
2016-11-19 15:09:10 +0200

    创建的项目闪光

---
+++ exercise 6/projectFlash/projectFlash/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 18.11.2016 8.41.58
+ * Author : jaska
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
